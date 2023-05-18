18f08fef276cde4376c34a9880eb3e8e42ec0efd
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-05-17 20:18:37 -0400

    spelling: compliance with the
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/test/java/org/openrewrite/analysis/controlflow/ControlFlowTest.java
+++ src/test/java/org/openrewrite/analysis/controlflow/ControlFlowTest.java
@@ -2,7 +2,7 @@
  * Copyright 2022 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in complianceWiththe License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
  * https://www.apache.org/licenses/LICENSE-2.0
