61786788d6bd1637e6ca64ef853108ac43729dc4
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2023-05-26 10:43:06 -0400

    Cleanup build logic
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,7 @@ plugins {
 group = "org.openrewrite.meta"
 description = "Static code analysis APIs leveraging data flow, control flow, and other AST-based search"
 
-val rewriteVersion = if (project.hasProperty("releasing")) {
-    "latest.release"
-} else {
-    "latest.integration"
-}
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 
 dependencies {
     compileOnly("org.projectlombok:lombok:latest.release")
