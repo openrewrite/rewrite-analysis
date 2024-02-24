2308f3bda66eb6eb8875201eed4a94132775c44b
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2024-02-24 14:54:34 -0500

    `CursorUtil.findCursorForTree`
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- src/main/java/org/openrewrite/analysis/util/CursorUtil.java
+++ src/main/java/org/openrewrite/analysis/util/CursorUtil.java
@@ -16,11 +16,16 @@
 package org.openrewrite.analysis.util;
 
 import fj.data.Option;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.openrewrite.Cursor;
+import org.openrewrite.Incubating;
+import org.openrewrite.Tree;
+import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.tree.J;
 
 import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicReference;
 
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class CursorUtil {
@@ -44,6 +49,35 @@ public class CursorUtil {
             }
         }
         return Option.none();
+    }
+
+
+    /**
+     * Find's a {@link Cursor} with a correctly constructed parent relationship for a given tree.
+     *
+     * @param start A cursor to start the search from
+     * @param tree The tree to find a cursor instance for.
+     * @return An option containing the cursor instance if found, otherwise none.
+     */
+    @Incubating(since = "2.4.0")
+    public static Option<Cursor> findCursorForTree(Cursor start, Tree tree) {
+        AtomicReference<Cursor> found = new AtomicReference<>();
+        new TreeCursorFinderVisitor(tree).visitNonNull(start.getParentTreeCursor().getValue(), found, start);
+        return Option.fromNull(found.get());
+    }
+
+    @AllArgsConstructor
+    private static class TreeCursorFinderVisitor extends TreeVisitor<Tree, AtomicReference<Cursor>> {
+        private final Tree tree;
+
+        @Override
+        public Tree preVisit(Tree tree, AtomicReference<Cursor> cursorAtomicReference) {
+            if (this.tree.isScope(tree)) {
+                cursorAtomicReference.set(getCursor());
+                stopAfterPreVisit();
+            }
+            return super.preVisit(tree, cursorAtomicReference);
+        }
     }
 
 }
---
+++ src/test/java/org/openrewrite/analysis/util/CursorUtilTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.analysis.util;
+
+import fj.data.Option;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.Cursor;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.test.RewriteTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.openrewrite.java.Assertions.java;
+
+public class CursorUtilTest implements RewriteTest {
+
+    private static final class FindCursorForTreeTestVisitor extends JavaVisitor<ExecutionContext> {
+
+        @Override
+        public J visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
+            Option<Cursor> found = CursorUtil.findCursorForTree(getCursor(), method.getArguments().get(0));
+
+            assertTrue(found.isSome(), "Cursor for the sub-tree tree not found");
+            assertThat(found.some().<J>getValue()).isEqualTo(method.getArguments().get(0));
+            assertThat(found.some().dropParentUntil(J.MethodInvocation.class::isInstance).<J>getValue()).isEqualTo(method);
+            assertThat(found.some().dropParentUntil(J.CompilationUnit.class::isInstance)).isNotNull();
+
+            return super.visitMethodInvocation(method, executionContext);
+        }
+    }
+
+    @Test
+    public void findCursorForTreeTest() {
+        rewriteRun(
+          spec -> spec.recipe(RewriteTest.toRecipe(FindCursorForTreeTestVisitor::new)),
+          java(
+            """
+            class Test {
+                void test() {
+                    System.out.println("Hello, World!");
+                }
+            }
+            """
+          )
+        );
+    }
+}
